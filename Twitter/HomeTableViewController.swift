f2422f1ca8b2db7db40e0787ba6f4fd4635f2b20
Alexis Vu <alexis.lauren.vu@gmail.com>
2020-02-12 15:53:46 -0800

    Added favorite and retweet.

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -17,12 +17,17 @@ class HomeTableViewController: UITableViewController {
         super.viewDidLoad()
         loadTweet()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweet()
+    }
 
     func loadTweet() {
         let url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let params = ["count": 10]
         
-        
+
         TwitterAPICaller.client?.getDictionariesRequest(url: url, parameters: params, success: { (tweets: [NSDictionary]) in
             // clean list up and repopulate
             self.tweetArray.removeAll()
@@ -61,8 +66,18 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
+       // set favorite
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
+        
         return cell
     }
+    
+   
+    
+    
     
     // MARK: - Table view data source
 
